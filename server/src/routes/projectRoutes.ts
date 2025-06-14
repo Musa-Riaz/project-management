6a51e9231935b64202d0f09e05f1e633ea01fd05
Musariaz <riazmusa69@gmail.com>
2025-06-14 11:44:31 +0500

    initialize

---
+++ server/src/routes/projectRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { getProjects, createProjects } from "../controllers/projectController";
+
+const router = Router();
+router.get("/", getProjects);
+router.post("/", createProjects);
+
+export const projectRoutes = router;
