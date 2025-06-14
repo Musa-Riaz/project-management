0b2806106e4bfcce4d6a7392990c658b7dc08ce0
Musariaz <riazmusa69@gmail.com>
2025-06-14 16:58:52 +0500

    added the snack bar, and delete projet feature

--- server/src/controllers/projectController.ts
+++ server/src/controllers/projectController.ts
@@ -14,7 +14,39 @@ export const getProjects = async (req:Request, res: Response) : Promise<void> =>
     }
 }
 
-export const createProjects = async (req: Request, res: Response): Promise<void> => {
+export const getProjectById = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {projectId} = req.params;
+        const project = await prisma.project.findUnique({
+            where: {
+                id: Number(projectId)
+            }
+        })
+        res.status(200).json(project);
+    }
+    catch(err: any){
+           res.status(500).json({
+            message: `Error fetching the project: ${err.message}`
+        });
+        console.error(err);
+    
+    }
+}
+
+export const createProjects = async (req: Request, res: Response): Promise<any> => {
+
+    // first check if the project already exists or not
+    const existingProject = await prisma.project.findFirst({
+        where: {
+            name: req.body.name
+        }
+    })
+    if (existingProject) {
+      return res.status(400).json({ 
+        success:false,
+        message: 'Project already exists.' });
+    }
+
     const {name, description, startDate, endDate} = req.body;
     try{
         const newProject = await prisma.project.create({
@@ -25,11 +57,32 @@ export const createProjects = async (req: Request, res: Response): Promise<void>
                 endDate
             }
         });
-        res.status(201).json(newProject);
+        res.status(201).json({success:true,newProject});
     }
     catch(err: any){
         res.status(500).json({
             message: `Error creating projects: ${err.message}`
+        });
+        console.error(err);
+    }
+}
+
+export const deleteProject = async (req: Request, res: Response) : Promise<void> => {
+    const { projectId } = req.params;
+    try {
+        const deleteProject = await prisma.project.delete({
+            where: {
+                id: Number(projectId)
+            }
+        })
+        res.status(200).json({
+            message: `Project with id ${projectId} deleted successfully`,
+            project: deleteProject
+        })
+    }
+    catch(err: any) {
+    res.status(500).json({
+            message: `Error deleting the project: ${err.message}`
         });
         console.error(err);
     }
